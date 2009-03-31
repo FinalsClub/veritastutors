811982320d31948a98c1b4b329f7228bf99fa519
Nicholas Hanssens <q@qwidjibo.org>
2009-03-31 11:59:57 -0400

    added static role instances

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,9 @@ class Role < ActiveRecord::Base
   ClientId = 2
   StudentId = 3
   TutorId = 4
+
+  Admin = Role.find_by_id(AdminId)
+  Client = Role.find_by_id(ClientId)
+  Student = Role.find_by_id(StudentId)
+  Tutor = Role.find_by_id(TutorId)
 end
