d1da2f13f619405d964b3bc5c3df62425a670388
Nicholas Hanssens <q@qwidjibo.org>
2009-03-31 12:00:51 -0400

    cleanup commit

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090331000300) do
+ActiveRecord::Schema.define(:version => 20090331155330) do
 
   create_table "appointment_requests", :force => true do |t|
     t.integer  "tutor_id"
