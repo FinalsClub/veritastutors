6783b6e839da58493ae0e3a39baf8bd265a6fc0e
Nicholas Hanssens <q@qwidjibo.org>
2009-03-30 20:30:59 -0400

    fixed bug in db migration

--- db/migrate/20090331000300_convert_role_ids.rb
+++ db/migrate/20090331000300_convert_role_ids.rb
@@ -2,14 +2,29 @@ class ConvertRoleIds < ActiveRecord::Migration
   def self.up
     Role.all.each { |r| r.delete }
 
-    Role.create :id => Role::TutorId, :name => "tutor"
-    Role.create :id => Role::AdminId, :name => "admin"
-    Role.create :id => Role::ClientId, :name => "client"
-    Role.create :id => Role::StudentId, :name => "student"    
+    r = Role.new :name => "tutor"
+    r.id = Role::TutorId
+    r.save
+
+    r = Role.new :name => "client"
+    r.id = Role::ClientId
+    r.save
+
+    r = Role.new :name => "student"
+    r.id = Role::StudentId
+    r.save
+
+    r = Role.new :name => "admin"
+    r.id = Role::AdminId
+    r.save
     
   end
 
   def self.down
+
+    Role.all.each { |r| r.delete }
+
+
     Role.create :name => "tutor"
     Role.create :name => "admin"
     Role.create :name => "client"
